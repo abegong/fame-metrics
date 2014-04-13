1a9c2ecd8211aa598439fdd5d3815a654d1d7cd4
Abe Gong <abegong@gmail.com>
2014-04-13 14:41:10 -0700

    [X]Clean up js cruft [X]Refactor session info out of mclbiq [X]Add session info: browser, OS [X]Add session index [X]Store sessions in firebase

--- app/data/data.js
+++ app/data/data.js
@@ -1,24 +1,4 @@
-rights = [
-	'Bingo!',
-	'Yes!',
-	'Correct',
-	'Nailed it',
-	'Right',
-	'You got it',
-	'Check',
-	'+1',
-	'Affirmative'
-];
-
-wrongs = [
-	'Nope',
-	'Wrong',
-	'Miss',
-	'Missed it',
-	'Incorrect',
-	'Sorry'
-];
-
+rights = [ 'Bingo!', 'Yes!', 'Correct', 'Nailed it', 'Right', 'You got it', 'Check', '+1', 'Affirmative'];
+wrongs = [ 'Nope', 'Wrong', 'Miss', 'Missed it', 'Incorrect', 'Sorry'];
 famous_adjs = [ 'acclaimed', 'brilliant', 'distinguished', 'eminent', 'excellent', 'glorious', 'grand', 'great', 'honored', 'illustrious', 'important', 'influential', 'leading', 'memorable', 'noted', 'notorious', 'outstanding', 'powerful', 'preeminent', 'prominent', 'recognized', 'remarkable', 'renowned', 'splendid', 'well-known', 'applauded', 'august', 'celebrated', 'conspicuous', 'elevated', 'exalted', 'extraordinary', 'foremost', 'imposing', 'lionized', 'mighty', 'publicized', 'noble', 'noteworthy', 'peerless', 'reputable', 'signal', 'talked-about'];
-
 famous_nouns = [ 'figure', 'hero', 'luminary', 'personage', 'personality', 'someone', 'star', 'superstar', 'ace', 'bigwig', 'cynosure', 'heavyweight', 'hotshot', 'immortal', 'lion', 'magnate', 'mahatma', 'name', 'notable', 'somebody', 'VIP', 'worthy', 'big-cheese', 'big-shot', 'celeb', 'major-leaguer'];
